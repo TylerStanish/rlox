183c83d08a0dec8b51cc6163cf681d05792ed9ed
TylerStanish <tystanish@gmail.com>
2020-03-20 09:13:55 -0400

    changed Expression from trait to enum

--- src/expressions.rs
+++ src/expressions.rs
@@ -35,160 +35,50 @@ impl From<LoxObject> for bool {
     }
 }
 
-pub trait Expression: Statement + fmt::Display {
-    fn eval(&self) -> LoxObject;
+pub enum Expression {
+    ExprBinary(Token, Box<Expression>, Box<Expression>),
+    ExprUnary(Token, Box<Expression>),
+    ExprLiteral(Token),
+    ExprGrouping(Box<Expression>),
 }
 
-/*
-pub trait ExpressionAsStatement {
-    fn as_statement(&self) -> &dyn Statement;
-}
-
-impl<T> ExpressionAsStatement for T where T: Expression {
-    fn as_statement(&self) -> &dyn Statement {
-        self
-    }
-}
-*/
-
-// also, see this: https://stackoverflow.com/a/29256897
-impl<T> Statement for T where T: Expression {
-    fn eval_statement(&self) {
-        self.eval();
-    }
-}
-
-pub struct BinaryExpression {
-    left: Box<dyn Expression>,
-    operator: Token,
-    right: Box<dyn Expression>,
-}
-
-impl BinaryExpression {
-    pub fn new(left: Box<dyn Expression>, operator: Token, right: Box<dyn Expression>) -> Self {
-        BinaryExpression {
-            left,
-            operator,
-            right,
+impl Expression {
+    pub fn eval(&self) -> LoxObject {
+        match self {
+            Expression::ExprBinary(op_tok, lhs, rhs) => match op_tok.token_type {
+                TokenType::Plus => {
+                    let left_num: f64 = lhs.eval().into();
+                    let right_num: f64 = rhs.eval().into();
+                    LoxObject::LoxNumber(left_num + right_num)
+                },
+                TokenType::Minus => {
+                    let left_num: f64 = lhs.eval().into();
+                    let right_num: f64 = rhs.eval().into();
+                    LoxObject::LoxNumber(left_num - right_num)
+                },
+                TokenType::Star => {
+                    let left_num: f64 = lhs.eval().into();
+                    let right_num: f64 = rhs.eval().into();
+                    LoxObject::LoxNumber(left_num * right_num)
+                },
+                TokenType::Slash => {
+                    let left_num: f64 = lhs.eval().into();
+                    let right_num: f64 = rhs.eval().into();
+                    LoxObject::LoxNumber(left_num / right_num)
+                },
+            }
+            Expression::ExprUnary(op_tok, operand) => match op_tok.token_type {
+                TokenType::Bang => {
+                    let res: bool = operand.eval().into();
+                    LoxObject::LoxBoolean(res)
+                },
+                TokenType::Minus => {
+                    let res: f64 = operand.eval().into();
+                    LoxObject::LoxNumber(-res)
+                },
+            },
+            Expression::ExprGrouping(expr) => expr.eval(),
+            Expression::ExprLiteral(literal) => literal.eval(),
         }
-    }
-}
-
-// https://doc.rust-lang.org/book/ch19-03-advanced-traits.html#using-supertraits-to-require-one-traits-functionality-within-another-trait
-impl fmt::Display for BinaryExpression {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "({} {} {})", self.operator, self.left, self.right)
-    }
-}
-
-impl Expression for BinaryExpression {
-    fn eval(&self) -> LoxObject {
-        match self.operator.token_type {
-            TokenType::Plus => {
-                let left_num: f64 = self.left.eval().into();
-                let right_num: f64 = self.right.eval().into();
-                LoxObject::LoxNumber(left_num + right_num)
-            },
-            TokenType::Minus => {
-                let left_num: f64 = self.left.eval().into();
-                let right_num: f64 = self.right.eval().into();
-                LoxObject::LoxNumber(left_num - right_num)
-            },
-            TokenType::Star => {
-                let left_num: f64 = self.left.eval().into();
-                let right_num: f64 = self.right.eval().into();
-                LoxObject::LoxNumber(left_num * right_num)
-            },
-            TokenType::Slash => {
-                let left_num: f64 = self.left.eval().into();
-                let right_num: f64 = self.right.eval().into();
-                LoxObject::LoxNumber(left_num / right_num)
-            },
-            _ => panic!(format!("Expected one of ['+', '-', '*', '/'] for binary operation, got {}", self.operator.token_type)), // runtime error (not our fault! user's fault!)
-        }
-    }
-}
-
-pub struct UnaryExpression {
-    operator: Token,
-    operand: Box<dyn Expression>,
-}
-
-impl UnaryExpression {
-    pub fn new(operator: Token, operand: Box<dyn Expression>) -> Self {
-        UnaryExpression { operator, operand }
-    }
-}
-
-impl fmt::Display for UnaryExpression {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "({} {})", self.operator, self.operand)
-    }
-}
-
-impl Expression for UnaryExpression {
-    fn eval(&self) -> LoxObject {
-        match self.operator.token_type {
-            TokenType::Minus => {
-                let operand: f64 = self.operand.eval().into();
-                LoxObject::LoxNumber(-operand)
-            },
-            TokenType::Bang => {
-                let operand: bool = self.operand.eval().into();
-                LoxObject::LoxBoolean(!operand)
-            },
-            _ => panic!(format!("Expected one of ['-', '!'] for binary operation, got {}", self.operator.token_type)), // runtime error (not our fault! user's fault!)
-        }
-    }
-}
-
-pub struct LiteralExpression {
-    literal: Token,
-}
-
-impl LiteralExpression {
-    pub fn new(literal: Token) -> Self {
-        LiteralExpression { literal }
-    }
-}
-
-impl fmt::Display for LiteralExpression {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.literal)
-    }
-}
-
-impl Expression for LiteralExpression {
-    fn eval(&self) -> LoxObject {
-        match &self.literal.token_type {
-            TokenType::Number(num) => LoxObject::LoxNumber(*num),
-            TokenType::True => LoxObject::LoxBoolean(true),
-            TokenType::False => LoxObject::LoxBoolean(false),
-            TokenType::StringLiteral(s) => LoxObject::LoxString(s.clone()),
-            TokenType::Eof => LoxObject::LoxNil,
-            _ => panic!(format!("Expected a literal token type, got {}", self.literal.token_type)), // runtime error (not our fault! user's fault!)
-        }
-    }
-}
-
-pub struct GroupingExpression {
-    expression: Box<dyn Expression>,
-}
-
-impl GroupingExpression {
-    pub fn new(expression: Box<dyn Expression>) -> Self {
-        GroupingExpression { expression }
-    }
-}
-
-impl fmt::Display for GroupingExpression {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "({})", self.expression)
-    }
-}
-
-impl Expression for GroupingExpression {
-    fn eval(&self) -> LoxObject {
-        self.expression.eval()
     }
 }
